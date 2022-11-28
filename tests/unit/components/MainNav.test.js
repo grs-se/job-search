08cbbf29a6603ddbc4603c67e91f7fa614f5e1b3
grs-se <george_ricesmith@hotmail.com>
2022-11-28 14:32:42 +0000

    Add v-if and v-else for isLoggedIn ProfileImage component

---
+++ tests/unit/components/MainNav.test.js
@@ -0,0 +1,27 @@
+import { mount } from "@vue/test-utils";
+
+import MainNav from "@/components/MainNav.vue";
+
+describe("MainNav", () => {
+	it("displays company name", () => {
+		const wrapper = mount(MainNav);
+
+		expect(wrapper.text()).toMatch("Boogle Careers");
+	});
+
+	it("displays menu items for navigation", () => {
+		const wrapper = mount(MainNav);
+		const navigationMenuItems = wrapper.findAll(
+			"[data-test='main-nav-list-item']"
+		);
+		const navigationMenuTexts = navigationMenuItems.map((item) => item.text());
+		expect(navigationMenuTexts).toEqual([
+			"Teams",
+			"Locations",
+			"Life at Boogle",
+			"How we hire",
+			"Students",
+			"Jobs",
+		]);
+	});
+});
