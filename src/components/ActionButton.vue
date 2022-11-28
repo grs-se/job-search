c2b6f3e2a8ce5489e9e9313b756abbb2d37d732b
grs-se <george_ricesmith@hotmail.com>
2022-11-28 20:24:31 +0000

    Add 'text' prop to ActionButton

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,13 +1,21 @@
 <template>
-	<button
-		class="px-5 py-3 font-medium text-white border-0 rounded bg-brand-blue-1 hover:shadow-blue"
-	>
-		Sign in
+	<button class="primary">
+		{{ text }}
 	</button>
 </template>
 
 <script>
 export default {
 	name: "ActionButton",
+	props: ["text"],
 };
 </script>
+
+<style scoped>
+button {
+	@apply px-5 py-3 rounded font-medium;
+}
+.primary {
+	@apply text-white bg-brand-blue-1 hover:shadow-blue;
+}
+</style>
